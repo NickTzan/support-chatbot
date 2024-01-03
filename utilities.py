8fc8e8fb8c834ff12951f173a82a6b9d725d92ca
Nikolaos Tzanis <nicktzanis87@gmail.com>
2024-01-03 16:22:38 +0200

    adapted for pdf

--- utilities.py
+++ utilities.py
@@ -1,31 +1,13 @@
-import asyncio
-from langchain.document_loaders.sitemap import SitemapLoader
+from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 
 
 # Get data from the website's posts
-def get_post_data(sitemap_url):
+def read_pdf_data(pdf_path):
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loader = SitemapLoader(sitemap_url)
-    docs = loader.load()
-    return docs
-
-# Split post data into chunks
-def split_data(docs):
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200,
-        length_function = len
-    )
-    docs_chunks = text_splitter.split_documents(docs)
-    return docs_chunks
-
-# Create the embeddings instance
-def create_embeddings():
-
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = splitter.split_documents(documents)
     embeddings = SentenceTransformerEmbeddings(model_name = 'all-MiniLM-L6-v2')
-    return embeddings
